7986deeb68def665836be8b6ce5fd27b85e933df
BigBIueWhale <85248530+BigBIueWhale@users.noreply.github.com>
2023-03-25 23:57:06 +0300

    Much more stable
    GPT 3 is extremely bad at following instructions and not getting distracted. I now made the script make it clear to GPT 3 how it's supposed to behave, by example. I also had to make a content filtering mechanism so that if GPT 3 outputs something that isn't a terminal command (which it often does because it's bad at following instructions) then I don't put that into the history, because then the bad output from GPT distracts GPT in future requests.

--- sentient.py
+++ sentient.py
@@ -1,3 +1,5 @@
+GOAL = "#Script to open YouTube with the latest video by Hank Green"
+
 import os
 import ast
 import typing
@@ -21,7 +23,7 @@ def isValidPythonStatement(statement: str) -> bool:
   try:
     ast.parse(statement)
   except SyntaxError:
-    print(f"The statement to test: {statement} does not have the structure of a valid Python statement.")
+    print(f"\nThe statement to test: {statement} does not have the structure of a valid Python statement.", end="")
     return False
   return True
 
@@ -44,7 +46,7 @@ class MyConsole(code.InteractiveConsole):
     def __init__(self, locals=None, filename="<console>"):
         super().__init__(locals, filename)
         self.output = ""
-        self.prompt = "Continue this Python terminal output, user input is not an option."
+        self.prompt = ""
         self.callback_command_string: Callable[[str], None] = do_not_print_string
         self.callback_output_string: Callable[[str], None] = do_not_print_string
 
@@ -67,7 +69,7 @@ class MyConsole(code.InteractiveConsole):
               push_return_value = super().push(full_statement)
             self._flush_output(stdout_capture)
             if push_return_value != False:
-              self.append_output_to_prompt(f"Failed executing command in interactive terminal, code.InteractiveConsole.push() returned {push_return_value}.")
+              self.append_output_to_prompt(f"Failed executing command in interactive terminal")
         except Exception as e:
             self._flush_output(stdout_capture)
             self.append_output_to_prompt(trim_long_text(traceback.format_exc()))
@@ -95,6 +97,12 @@ class MyConsole(code.InteractiveConsole):
     def write(self, data):
         # Append the data to the output attribute
         self.output += data
+
+    def delete_last_command(self):
+      idx = self.prompt.rfind("\n>>>")
+      if idx >= 0:
+        self.callback_command_string(f"\nDELETE {self.prompt[idx+1:]}")
+        self.prompt = self.prompt[:idx]
    
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -102,6 +110,19 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 #engines = openai.Engine.list()
 #for engine in engines["data"]:
 #   print(f"{engine}")
+
+reset_passages =\
+[\
+"We've hit a dead end, it's time to pursue another strategy now.",
+"We've reached an impasse; it's now necessary to explore a different approach.",
+"We've come to a standstill; it's time to adopt a new tactic.",
+"Having reached a deadlock, we must now seek an alternative plan.",
+"We've encountered a roadblock; the moment has come to try a different method.",
+]
+
+def choose_reset_passage(seed: int) -> str:
+  return reset_passages[seed % len(reset_passages)]
+
 
 class SentientAi():
   def __init__(self):
@@ -115,7 +136,7 @@ class SentientAi():
     self.console.callback_command_string = print_string
     self.console.callback_output_string = print_string
 
-    self.console.push("#Script to run all exe files in folder_path")
+    self.console.push(GOAL)
     self.console.push("#First verify that duckduckgo-search is installed and that we can get basic search results:")
     self.console.push("import subprocess")
     self.console.push("command = \"python -m pip install duckduckgo-search\"")
@@ -128,21 +149,39 @@ class SentientAi():
     self.console.push("first_result_header: str = results[0][\"title\"]")
     self.console.push("first_result_body: str = results[0][\"body\"]")
     self.console.push("first_result_link: str = results[0][\"href\"]")
-    self.console.push("folder_path = r\"C:\\Users\\user\\Downloads\\\\\"")
-    self.console.push("#Now the full script to run all exe files in folder_path:")
+    self.console.push("#Now the full script itself:")
     
     self.console.callback_command_string = do_not_print_string
 
+  
   def loop(self):
-    for i in range(15):
+    for i in range(100):
       input("\nPress ENTER to call OpenAI")
       extended_prompt = self.console.prompt + "\n>>>"
+      messages=[
+        {"role": "system", "content": "You are an assistant, complete the code by adding statements. Learn from errors in the shell history. You can't change past statements, only add new ones."},
+        {"role": "user", "content": "I will provide a Python interactive shell terminal output, please respond with the very next most likely Python statement. Respond with nothing but code. Now answer a boolean value of whether you understand the instructions."},
+        {"role": "assistant", "content": "True"},
+        {"role": "user", "content": "Let's try an example:\n>>>#print hello\n>>>"},
+        {"role": "assistant", "content": "\"hello\""},
+        {"role": "user", "content": ">>>#print hello\n>>>\"hello\"\nhello\n>>>#Now let's count until 5\n>>>"},
+        {"role": "assistant", "content": "for i in range(5):\nprint(i)"},
+        {"role": "user", "content": ">>>#print hello\n>>>\"hello\"\nhello\n>>>#Now let's count until 5\n>>>" +\
+'''Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    exec("for i in range(5):\nprint(i)")
+  File "<string>", line 2
+    print(i)
+    ^^^^^
+IndentationError: expected an indented block after 'for' statement on line 1
+''' + ">>>"},
+        {"role": "assistant", "content": "for i in range(5):\n  print(i)"},
+        {"role": "user", "content": ">>>#print hello\n>>>\"hello\"\nhello\n>>>#Now let's count until 5\n>>>for i in range(5):\n  print(i)\n>>>1\n2\n3\n4\n5\n>>>#I see you understood the example. Let's continue now."},
+        {"role": "user", "content": extended_prompt}
+      ]
       response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        messages=[
-          {"role": "system", "content": "Run a single Python statements in the Python interactive shell and then stop responding."},
-          {"role": "user", "content": extended_prompt}
-        ],
+        messages=messages,
         temperature = self._temperature,
         frequency_penalty=0.9,
         max_tokens=len(extended_prompt) + 512,
@@ -153,13 +192,16 @@ class SentientAi():
         if self._invalid_statement_count >= self._unstuck_number:
           # Avoid getting stuck in a feedback loop
           self.console.callback_command_string = print_string
-          self.console.push("#That didn't work, here's some more code:")
+          if self._invalid_statement_count > self._unstuck_number:
+            # Avoid placing multiple unstuck sentences in a row to in the prompt.
+            self.console.delete_last_command()
+          self.console.push(f"#{choose_reset_passage(self._invalid_statement_count)} back to the code now:")
           self.console.callback_command_string = do_not_print_string
-          self._invalid_statement_count = 0
         continue
       
       first_python_statement: str = getFirstStatement(response_text)
-      input(f">>>{first_python_statement}")
+      input(f"\n>>>{first_python_statement}")
+      #print(f"\n>>>{first_python_statement}")
       if not self.console.push(first_python_statement):
         # Error occurred running the statement
         self._invalid_statement_count += 1
